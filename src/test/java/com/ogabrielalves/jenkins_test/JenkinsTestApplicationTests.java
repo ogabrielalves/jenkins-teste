1ccaee10ad6bc76bc551da6515c0f745226d3e4d
Gabriel Alves <61751506+ogabrielalves@users.noreply.github.com>
2024-07-29 11:27:21 -0300

    Add files via upload

---
+++ src/test/java/com/ogabrielalves/jenkins_test/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogabrielalves.jenkins_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
